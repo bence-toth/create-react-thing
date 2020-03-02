a3f04a3d17dc7dfe72d51b925611cfd7e70b9332
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2020-03-02 19:39:19 +0100

    Fixed linter errors in utility

--- src/utility.js
+++ src/utility.js
@@ -4,52 +4,53 @@ const validateNpmPackageName = require('validate-npm-package-name');
 const capitalizeString = string => {
 	const firstLetter = `${string}`.charAt(0).toUpperCase();
 	const rest = `${string}`.slice(1);
-  return `${firstLetter}${rest}`;
+	return `${firstLetter}${rest}`;
 };
 
 const validatePackageName = packageName => {
 	const {errors} = validateNpmPackageName(packageName);
 	return (
-		errors
-			? errors.map(capitalizeString)
-			: undefined
-	)
+		errors ?
+			errors.map(capitalizeString) :
+			undefined
+	);
 };
 
 const validateScopeName = packageName => {
 	const {errors: npmErrors} = validateNpmPackageName(
-		packageName.substring(1)
-	)
+		packageName.slice(1)
+	);
 	const updatedNpmErrors = npmErrors && npmErrors.map(error => {
 		if (error === 'name length must be greater than zero') {
-			return 'name length must be greater than one'
+			return 'name length must be greater than one';
 		}
-		return error
-	})
+
+		return error;
+	});
 	const errors = [
 		...(
-			(packageName.length === 0)
-				? ['name length must be greater than zero']
-				: []
+			(packageName.length === 0) ?
+				['name length must be greater than zero'] :
+				[]
 		),
 		...(
-			(packageName && (packageName.charAt(0) !== '@'))
-				? (['name must start with an @'])
-				: []
+			(packageName && (packageName.charAt(0) !== '@')) ?
+				(['name must start with an @']) :
+				[]
 		),
 		...(
-			(packageName && packageName.includes('/'))
-				? (['name must not contain a /'])
-				: []
+			(packageName && packageName.includes('/')) ?
+				(['name must not contain a /']) :
+				[]
 		),
 		...(updatedNpmErrors || [])
-	]
+	];
 	return (
-		(errors.length > 0)
-			? errors.map(capitalizeString)
-			: undefined
-	)
-}
+		(errors.length > 0) ?
+			errors.map(capitalizeString) :
+			undefined
+	);
+};
 
 const getLoggedInNpmUsername = async () => {
 	const {stdout} = await execa('npm', ['whoami']);
