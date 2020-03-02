8c7e54f2b7bdb12decc52d0e3d7f843cb71ec284
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2020-03-02 16:53:41 +0100

    Added keywords input component

---
+++ src/keywordsInput.js
@@ -0,0 +1,59 @@
+const React = require('react');
+const {string, func, oneOf, arrayOf} = require('prop-types');
+const {stepStates} = require('./enum');
+const importJsx = require('import-jsx');
+
+const InputStep = importJsx('./inputStep.js')
+
+const {useEffect, useState} = React;
+
+const {current} = stepStates;
+
+const KeywordsInput = ({
+	onNextStep,
+	state,
+	keywords,
+	onSetKeywords
+}) => {
+	const [keywordsInput, onSetKeywordsInput] = useState('')
+	return (
+		<InputStep
+			state={state}
+			label='Keyword (comma-separated)'
+			value={(
+				(state === current)
+					? keywordsInput
+					: keywords.join(', ')
+			)}
+			fallback='blank'
+			onChange={newKeywordsValue => {
+				onSetKeywordsInput(newKeywordsValue)
+				onSetKeywords(
+					newKeywordsValue
+						.toLowerCase()
+						.split(',')
+						.map(keyword => keyword.trim())
+						.filter(keyword => (keyword.length > 0))
+						.filter((keyword, keywordIndex, allKeywords) => (
+							allKeywords.indexOf(keyword) === keywordIndex
+						))
+				)
+			}}
+			onSubmit={() => {
+				onNextStep();
+			}}
+		>
+			{'Keywords help people discover your package'}
+			{'as it’s listed in npm search.'}
+		</InputStep>
+	)
+}
+
+KeywordsInput.propTypes = {
+	onNextStep: func,
+	state: oneOf(Object.values(stepStates)),
+	keywords: arrayOf(string),
+	onSetKeywords: func
+}
+
+module.exports = KeywordsInput;
