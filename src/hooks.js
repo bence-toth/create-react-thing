05231073bdc210f4021fabf834f9170279f57f9c
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2020-03-02 19:36:58 +0100

    Fixed linter errors in hooks

--- src/hooks.js
+++ src/hooks.js
@@ -2,23 +2,23 @@ const {useState, useEffect} = require('react');
 const {getLoggedInNpmUsername} = require('./utility');
 
 const useNpmUsername = () => {
-  // Initialize it with empty string
-	[npmUsername, onSetNpmUsername] = useState('');
+	// Initialize it with empty string
+	const [npmUsername, onSetNpmUsername] = useState('');
 
-  // See if a user is logged in
-  useEffect(() => {
-		getLoggedInNpmUsername()
-			.then(username => {
-				if (username.length > 0) {
-					onSetNpmUsername(username);
-				}
-			});
-  }, []);
+	// See if a user is logged in
+	useEffect(() => {
+		(async () => {
+			const username = await getLoggedInNpmUsername();
+			if (username.length > 0) {
+				onSetNpmUsername(username);
+			}
+		})();
+	}, []);
 
-  // Expose value
-  return npmUsername;
-}
+	// Expose value
+	return npmUsername;
+};
 
 module.exports = {
-  useNpmUsername
+	useNpmUsername
 };
