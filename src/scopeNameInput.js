cfb947c7f2fb8db2da022ed06b9d847b56cd1bf3
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2020-03-02 09:50:16 +0100

    Added scope name step

---
+++ src/scopeNameInput.js
@@ -0,0 +1,91 @@
+const React = require('react');
+const {string, func, oneOf} = require('prop-types');
+const {Box, Text, Color} = require('ink');
+const {default: Input} = require('ink-text-input');
+const {validateScopeName} = require('./utility')
+
+const {useEffect, useState} = React;
+
+const states = {
+	upcoming: 'upcoming',
+	current: 'current',
+	completed: 'completed'
+};
+
+const {current, completed} = states;
+
+const ScopeNameInput = ({
+	onSetStep,
+	state,
+	scopeName,
+	onSetScopeName
+}) => {
+	const [isDirty, onSetIsDirty] = useState(false)
+	useEffect(() => {
+		if (state === current) {
+			onSetIsDirty(false);
+		}
+	}, [state]);
+	const [validationErrors, onSetValidationErrors] = useState();
+	useEffect(() => {
+		onSetValidationErrors(validateScopeName(scopeName))
+	}, [scopeName])
+	return (
+		<React.Fragment>
+			{(state === current) && (
+				<Box flexDirection="column">
+					<Box flexDirection="row">
+						<Text>- </Text>
+						<Text bold>npm package scope name: </Text>
+						<Input
+							value={scopeName}
+							onChange={value => {
+								onSetIsDirty(true);
+								onSetScopeName(value);
+							}}
+							onSubmit={() => {
+								onSetIsDirty(true);
+								if (!validationErrors) {
+									onSetStep(4);
+								}
+							}}
+						/>
+					</Box>
+					{isDirty && validationErrors && (
+						<Box paddingTop={1}>
+							<Text>
+								<Color bgKeyword='red'>
+									[!]
+								</Color>
+								{' '}
+								{validationErrors[0]}
+							</Text>
+						</Box>
+					)}
+				</Box>
+			)}
+			{(state === completed) && (
+				<Box flexDirection="row">
+					<Text>
+						<Color green>
+							{'✓ '}
+						</Color>
+					</Text>
+					<Text>npm package scope name: </Text>
+					<Text>{scopeName}</Text>
+				</Box>
+			)}
+		</React.Fragment>
+	)
+}
+
+ScopeNameInput.states = states
+
+ScopeNameInput.propTypes = {
+	onSetStep: func,
+	state: oneOf(Object.values(ScopeNameInput.states)),
+	scopeName: string,
+	onSetScopeName: func
+}
+
+module.exports = ScopeNameInput;
